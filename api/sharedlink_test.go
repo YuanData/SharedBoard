bf06339903213b2ec0338a51a73fad850d7ac9e5
Alan <kcykey@gmail.com>
2023-08-29 21:35:37 +0800

    Modify to table-driven test set to increase coverage

--- api/sharedlink_test.go
+++ api/sharedlink_test.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"bytes"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -25,25 +26,89 @@ func randomSharedlink() db.Sharedlink {
 }
 func TestGetSharedlinkAPI(t *testing.T) {
 	sharedlink := randomSharedlink()
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
+	testCases := []struct {
+		name          string
+		SharedlinkID  int64
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(recoder *httptest.ResponseRecorder)
+	}{
+		{
+			name:         "OK",
+			SharedlinkID: sharedlink.ID,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetSharedlink(gomock.Any(), gomock.Eq(sharedlink.ID)).
+					Times(1).
+					Return(sharedlink, nil)
 
-	store := mockdb.NewMockStore(ctrl)
-	store.EXPECT().
-		GetSharedlink(gomock.Any(), gomock.Eq(sharedlink.ID)).
-		Times(1).
-		Return(sharedlink, nil)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+				requireBodyMatchSharedlink(t, recorder.Body, sharedlink)
+			},
+		},
+		{
+			name:         "NotFound",
+			SharedlinkID: sharedlink.ID,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetSharedlink(gomock.Any(), gomock.Eq(sharedlink.ID)).
+					Times(1).
+					Return(db.Sharedlink{}, sql.ErrNoRows)
 
-	server := NewServer(store)
-	recorder := httptest.NewRecorder()
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusNotFound, recorder.Code)
+			},
+		},
+		{
+			name:         "InternalError",
+			SharedlinkID: sharedlink.ID,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetSharedlink(gomock.Any(), gomock.Eq(sharedlink.ID)).
+					Times(1).
+					Return(db.Sharedlink{}, sql.ErrConnDone)
 
-	url := fmt.Sprintf("/sharedlink/%d", sharedlink.ID)
-	request, err := http.NewRequest(http.MethodGet, url, nil)
-	require.NoError(t, err)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
+		{
+			name:         "InvalidID",
+			SharedlinkID: 0,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetSharedlink(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+	}
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.name, func(t *testing.T) {
 
-	server.router.ServeHTTP(recorder, request)
-	require.Equal(t, http.StatusOK, recorder.Code)
-	requireBodyMatchSharedlink(t, recorder.Body, sharedlink)
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mockdb.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			server := NewServer(store)
+			recorder := httptest.NewRecorder()
+
+			url := fmt.Sprintf("/sharedlink/%d", tc.SharedlinkID)
+			request, err := http.NewRequest(http.MethodGet, url, nil)
+			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, request)
+			tc.checkResponse(recorder)
+		})
+	}
 }
 
 func requireBodyMatchSharedlink(t *testing.T, body *bytes.Buffer, trader db.Sharedlink) {
